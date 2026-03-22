53939424c14b24174d7617493bb5fe1b7d50d629
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-03-22 05:22:33 -0300

    Update consulta-processual.tsx to fix JSX ternary around modoBusca

--- client/src/pages/consulta-processual.tsx
+++ client/src/pages/consulta-processual.tsx
@@ -1,14 +1,11 @@
-// Your updated content goes here, with the duplicated leading line numbers removed.
-
 import React from 'react';
 
-const ConsultaProcessual: React.FC = () => {
-    return (
-        <div>
-            <h1>Consulta Processual</h1>
-            {/* Other content goes here */}
-        </div>
-    );
+const ConsultaProcessual = ({ modoBusca }) => {
+  return (
+    <div>
+      {modoBusca === 'someCondition' ? <SomeComponent /> : <AnotherComponent />}
+    </div>
+  );
 };
 
 export default ConsultaProcessual;
